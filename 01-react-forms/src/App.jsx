b9d56cb5808542167bce26f8432606649bc67e3c
jesualujan <lujan.oficial@gmail.com>
2023-09-11 21:41:46 -0600

    instalación de React + Vite

---
+++ 01-react-forms/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ReactHookForm from './Components/ReactHookForm/ReactHookForm'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+
+function App() {
+  return (
+    <>
+      <ReactHookForm/>
+      <SimpleForm/>
+    </>
+  )
+}
+
+export default App
