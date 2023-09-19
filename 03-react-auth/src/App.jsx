fdbedb49582823b44ff97181e06d4b64227776e9
jesualujan <lujan.oficial@gmail.com>
2023-09-18 21:47:31 -0600

    ✨importamos las páginas y el componente

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { Home, Dashboard, Loggin, Secret, Signup } from '@/Pages'
+import Header from '@/Components/Header'
 
 function App () {
   return (
     <>
+      <Header />
       <p>React Auth</p>
+      <Home />
+      <Dashboard />
+      <Loggin />
+      <Secret />
+      <Signup />
     </>
   )
 }
