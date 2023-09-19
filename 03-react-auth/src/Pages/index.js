504c599d3270665747eaabfafd7c1d752173f1b3
jesualujan <lujan.oficial@gmail.com>
2023-09-18 21:48:31 -0600

    ✨ hacemos import y export por separado

---
+++ 03-react-auth/src/Pages/index.js
@@ -0,0 +1,14 @@
+import Dashboard from '@/Pages/Dashboard'
+import Home from '@/Pages/Home'
+import Loggin from '@/Pages/Loggin'
+import Secret from '@/Pages/Secret'
+import Signup from '@/Pages/Signup'
+
+// EXPORTAR COMO UN OBJETO
+export {
+  Dashboard,
+  Home,
+  Loggin,
+  Secret,
+  Signup
+}
