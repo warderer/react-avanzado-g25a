c1c797f7b172d6ac381a7fad4ce08a721a095b48
César Guerra <warderer@gmail.com>
2023-09-25 21:13:51 -0600

    ✨ Dashboard con Info del Usuario

--- 03-react-auth/src/Pages/Dashboard.jsx
+++ 03-react-auth/src/Pages/Dashboard.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { getMeUserService } from '@/services/userServices'
 
 const Dashboard = () => {
+  const [userData, setUserData] = useState({})
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getMeUserService(token)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error en Dashboard', error)
+      }
+    }
+    fetchUserData()
+  }, [token])
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      {
+        userData?.first_name && <p>{userData.first_name}</p>
+      }
+      {
+        userData?.last_name && <p>{userData.last_name}</p>
+      }
+      {
+        userData?.gender && <p>{userData.gender}</p>
+      }
+      {
+        userData?.email && <p>{userData.email}</p>
+      }
+      {
+        userData?.role && <p>{userData.role}</p>
+      }
+    </>
   )
 }
 
