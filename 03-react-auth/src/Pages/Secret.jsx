8075e00d4f70bd232b282921c5defef93d00d6e2
César Guerra <warderer@gmail.com>
2023-09-25 20:53:27 -0600

    ✨ Renderizado Condicional segun tipo de usuario

--- 03-react-auth/src/Pages/Secret.jsx
+++ 03-react-auth/src/Pages/Secret.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useAuthContext } from '@/hooks/useAuthContext'
 
 const Secret = () => {
+  const { userPayload } = useAuthContext()
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      {
+        userPayload?.role === 'ADMIN'
+          ? <h2>Hola ADMIN</h2>
+          : <h2>Hola CUSTOMER</h2>
+      }
+
+      {userPayload?.role === 'ADMIN' && <h2>Saludos ADMIN</h2>}
+
+      {userPayload?.role === 'CUSTOMER' && <h2>Saludos CUSTOMER</h2>}
+    </>
   )
 }
 
